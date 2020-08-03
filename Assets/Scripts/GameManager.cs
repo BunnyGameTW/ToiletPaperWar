3084f21b07380dff956f79f818ea85b44ab9b007
AngelCheng <angelcheng0916@gmail.com>
2020-08-04 01:33:55 +0800

    add temp ui, add player input

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum PlayerType
+{
+    CAT, HUMAN
+}
+
+
+
+public class GameManager : MonoBehaviour
+{
+    PlayerType winner;
+    public float gameTime;
+    public Text timeText;
+    public Image leftBarImage, rightBarImage;
+    public Player cat, human;
+
+    float timer;
+    float toiletValue;
+    const float toiletPaperRatio = 0.5f;
+    const float MAX_TOILET_VALUE = 100.0f;
+    const float MIN_TOILET_VALUE = 0.0f;
+    bool isGameOver;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        toiletValue = toiletPaperRatio * MAX_TOILET_VALUE;
+        isGameOver = false;
+        cat.attackEvent += OnPlayerAttack;
+        human.attackEvent += OnPlayerAttack;
+        cat.skillEvent += OnPlayerSkill;
+        human.skillEvent += OnPlayerSkill;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isGameOver)
+        {
+            float fillValue = toiletValue / MAX_TOILET_VALUE;
+            cat.SetAddSkillValue(1.0f - fillValue);
+            human.SetAddSkillValue(fillValue);
+
+            RefreshTimer();
+            UpdateUI();
+        }
+        else
+        {
+            //TODO 顯示贏家
+            Debug.Log("winner->" + winner.ToString());
+        }
+    }
+
+    void RefreshTimer()
+    {
+        gameTime -= Time.deltaTime; 
+        if(gameTime <= 0.0f)
+        {
+            gameTime = 0.0f;
+            isGameOver = true;
+            winner = toiletValue >= MAX_TOILET_VALUE / 2 ? PlayerType.CAT : PlayerType.HUMAN;
+        }
+    }
+
+    const float ATTACK_RATIO = 3.0f;
+
+    //event
+    void OnPlayerAttack(object sender, AttackEventArgs param)
+    {
+        float minus = param.playerType == PlayerType.CAT ? 1 : -1;
+        toiletValue += minus * param.attackValue * ATTACK_RATIO;
+        if (toiletValue >= MAX_TOILET_VALUE)
+        {
+            toiletValue = MAX_TOILET_VALUE;
+            winner = PlayerType.CAT;
+            isGameOver = true;
+        }
+        else if (toiletValue <= MIN_TOILET_VALUE)
+        {
+            toiletValue = MIN_TOILET_VALUE;
+            winner = PlayerType.HUMAN;
+            isGameOver = true;
+        }
+    }
+
+    void OnPlayerSkill(object sender, SkillEventArgs param)
+    {
+        if(param.playerType == PlayerType.CAT)
+        {
+            human.SetIsSkilled(true);
+        }
+        else
+        {
+            cat.SetIsSkilled(true);
+        }
+    }
+
+    void UpdateUI()
+    {
+        timeText.text = Mathf.Ceil(gameTime).ToString();
+        float fillValue = toiletValue / MAX_TOILET_VALUE;
+        leftBarImage.fillAmount = fillValue;
+        rightBarImage.fillAmount = 1.0f - fillValue;
+    }
+        
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class AttackEventArgs : EventArgs
+{
+    public float attackValue;
+    public PlayerType playerType;
+    public AttackEventArgs(float v, PlayerType t)
+    {
+        attackValue = v;
+        playerType = t;
+    }
+}
+
+public class SkillEventArgs : EventArgs
+{
+    public PlayerType playerType;
+    public SkillEventArgs(PlayerType t)
+    {
+        playerType = t;
+    }
+}
+
+public class Player : MonoBehaviour
+{
+   
+    public PlayerType type;
+    [Serializable]
+    public struct PlayerInput
+    {
+        public KeyCode attack;
+        public KeyCode skill;
+    }
+    public PlayerInput input;
+    public Image attackImage, skillImage;
+    public Sprite pressKey, normalKey, disableKey;
+
+    const float ATTACK_VALUE = 1.0f;//攻擊力
+    const float DECREASE_ATK_RATIO = 0.1f;//減緩攻擊比例
+    const int CAN_NUMBER = 10;//罐頭連打數
+    const float MAX_SKILL_VALUE = 100.0f;//技能最大值
+    const float ADD_SKILL_RATIO = 10.0f;//累積技能比例
+    const float DECREASE_TIME = 3.0f;//減緩時間
+
+    float skillValue, addSkillValue;
+    bool isSkilled;
+    float skillTimer;
+    int canCounter;
+    public event EventHandler<AttackEventArgs> attackEvent;
+    public event EventHandler<SkillEventArgs> skillEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        skillValue = addSkillValue = 0.0f;
+        skillTimer = 0.0f;
+        canCounter = 0;
+        isSkilled = false;
+        skillImage.sprite = disableKey;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckCatSkill();
+        UpdateSkillValue();
+        DetectInput();
+    }
+   
+    //public
+    //設定累積技能速度
+    public void SetAddSkillValue(float value)
+    {
+        addSkillValue = value;
+    }
+
+    //設定被攻擊
+    public void SetIsSkilled(bool boolean)
+    {
+        isSkilled = boolean;
+    }
+
+    //private
+    //更新技能值
+    void UpdateSkillValue()
+    {
+        skillValue += addSkillValue * ADD_SKILL_RATIO * Time.deltaTime;
+        if (skillValue >= MAX_SKILL_VALUE)
+        {
+            skillValue = MAX_SKILL_VALUE;
+            skillImage.sprite = normalKey;
+        }
+        skillImage.fillAmount = skillValue / MAX_SKILL_VALUE;
+    }
+
+    //偵測輸入
+    void DetectInput()
+    {
+        CheckAttack();
+        CheckSkill();
+    }
+  
+    void CheckAttack()
+    {
+        if (Input.GetKeyDown(input.attack))
+        {
+            float attackValue = GetAttackValue();
+            if (attackValue > 0)
+            {
+                attackEvent.Invoke(this, new AttackEventArgs(attackValue, type));
+            }
+            CheckHumanSkill();
+            attackImage.sprite = pressKey;
+        }
+        else if (Input.GetKeyUp(input.attack))
+        {
+            attackImage.sprite = normalKey;
+        }
+    }
+
+    void CheckSkill()
+    {
+        if (Input.GetKeyDown(input.skill) && skillValue == MAX_SKILL_VALUE)
+        {
+            skillImage.sprite = pressKey;
+            skillValue = 0.0f;
+            skillEvent.Invoke(this, new SkillEventArgs(type));
+        }
+        else if (Input.GetKeyUp(input.skill) && skillValue != MAX_SKILL_VALUE)
+        {
+            skillImage.sprite = disableKey;
+        }
+    }
+
+    //取得攻擊力
+    float GetAttackValue()
+    {
+        if (isSkilled)
+        {
+            return type == PlayerType.CAT ? 0.0f : ATTACK_VALUE * DECREASE_ATK_RATIO;
+        }
+        return ATTACK_VALUE;
+    }
+
+    //檢查人類技能
+    void CheckHumanSkill()
+    {
+        if (isSkilled && type == PlayerType.CAT)
+        {
+            canCounter++;
+            if (canCounter == CAN_NUMBER)
+            {
+                canCounter = 0;
+                isSkilled = false;
+            }
+        }
+    }
+
+    //檢查貓咪技能
+    void CheckCatSkill()
+    {
+        if (isSkilled && type == PlayerType.HUMAN)
+        {
+            skillTimer += Time.deltaTime;
+            if (skillTimer >= DECREASE_TIME)
+            {
+                skillTimer = 0.0f;
+                isSkilled = false;
+            }
+        }
+    }
+}
